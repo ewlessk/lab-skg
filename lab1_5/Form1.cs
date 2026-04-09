de4e3d1ee8f39918374a256d6794adedd20522b5
ewlessk <102576338+ewlessk@users.noreply.github.com>
2026-04-09 23:52:33 +0300

    Add files via upload

---
+++ lab1_5/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            label6.Text = Convert.ToString(a + b);
+            label7.Text = Convert.ToString(a - b);
+            label8.Text = Convert.ToString(a * b);
+
+        }
+    }
+}
