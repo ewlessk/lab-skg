8bdfd0d2116dfcb1bf10a5d77e53c8e840ecca19
ewlessk <102576338+ewlessk@users.noreply.github.com>
2026-04-17 00:46:21 +0300

    Add files via upload

---
+++ lab1_3/lab1_3/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public double a, b;
+        public char c;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "9";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "1";
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "00";
+        }
+        private void button12_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                a = System.Convert.ToDouble(textBox1.Text);
+                c = '+';
+                textBox1.Text = "";
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+        private void button13_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                a = System.Convert.ToDouble(textBox1.Text);
+                c = '-';
+                textBox1.Text = "";
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        private void button14_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                a = System.Convert.ToDouble(textBox1.Text);
+                c = '*';
+                textBox1.Text = "";
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+        private void button15_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                a = System.Convert.ToDouble(textBox1.Text);
+                c = '/';
+                textBox1.Text = "";
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        private void button16_Click(object sender, EventArgs e)
+        {
+            b = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "";
+            switch (c)
+            {
+                case '+':
+                    textBox1.Text = Convert.ToString(a + b); break;
+                case '-':
+                    textBox1.Text = Convert.ToString(a - b); break;
+                case '*':
+                    textBox1.Text = Convert.ToString(a * b); break;
+                case '/':
+                    textBox1.Text = Convert.ToString(a / b); break;
+            }
+        }
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += ",";
+        }
+        private void button18_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = Convert.ToString(Math.Pow(a, 2));
+        }
+        private void button19_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = Convert.ToString(Math.Sin((a * Math.PI) / 180));
+        }
+        private void button20_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = Convert.ToString(Math.Cos((a * Math.PI) / 180));
+        }
+        private void button21_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = Convert.ToString(Math.Tan((a * Math.PI) / 180));
+        }
+        private void button22_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = Convert.ToString(Math.Cos((a * Math.PI) / 180) / (Math.Sin((a * Math.PI) / 180)));
+        }
+        private void button23_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            a = 0; b = 0;
+        }
+        private void button24_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            s = s.Substring(0, s.Length - 1);
+            textBox1.Text = s;
+        }
+        private void button25_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = Convert.ToString(Math.Sqrt(a));
+        }
+    }
+
+}
--- lab2/lab2/Form1.Designer.cs
+++ lab2/lab2/Form1.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace lab_1_4
+﻿
+namespace lab2
 {
     partial class Form1
     {
@@ -28,93 +29,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
             this.SuspendLayout();
             // 
-            // label1
+            // listBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(52, 181);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(19, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "00";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(107, 181);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(10, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = ":";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(158, 181);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(19, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "00";
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Line",
+            "Rectangle",
+            "FillRectangle",
+            "Ellipse",
+            "FillEllipse",
+            "Pie",
+            "FillPie"});
+            this.listBox1.Location = new System.Drawing.Point(12, 51);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(77, 95);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(26, 197);
+            this.button1.Location = new System.Drawing.Point(54, 190);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Пуск";
+            this.button1.TabIndex = 1;
+            this.button1.Text = "цвет пера";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(130, 197);
+            this.button2.Location = new System.Drawing.Point(172, 190);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Сброс";
+            this.button2.TabIndex = 2;
+            this.button2.Text = "очистить";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // timer1
+            // button3
             // 
-            this.timer1.Interval = 500;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            this.button3.Location = new System.Drawing.Point(100, 236);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "выбрать цвет кисти";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
             this.Name = "Form1";
-            this.Text = "таймер";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ColorDialog colorDialog2;
     }
 }
 
